75364c40f6ebc086342817ba465ed1ea0bdd414c
minji <busy8863@naver.com>
2022-11-25 20:38:05 +0900

    Loadingするのにかかる進行度表示およびfavicon生成

--- js/shopList.js
+++ js/shopList.js
@@ -37,7 +37,7 @@ function doAjax(url, range, startNum) {
 
 // result of getting restaurant
 function successCall(data) {
-  console.log(data);
+  //console.log(data);
   let shops = data.results.shop;
   let cnt = data.results.results_available;
 
@@ -45,14 +45,21 @@ function successCall(data) {
     "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=3aed834ab74d67bd&lat=35.6773686&lng=139.7694908&format=jsonp" +
     "&callback=successCall&count=100";
   let range = $("#range").val();
-  let startNum = parseInt($("#startNum").text()) + 100; // 1 ~ 100, 101 ~ 200, 201 ~ 300, ....
-  $("#startNum").text(startNum); // 100以上　全ての情報を呼び出す
+  let startNum = parseInt($("#startNum").text()) + 100; // 1 ~ 100, 101 ~ 200, 201 ~ 300, .... 100以上　全ての情報を呼び出す
+  $("#startNum").text(startNum);
   dataSet.push.apply(dataSet, shops);
   console.log(url);
 
+  // データパースの進行度を表す
+  if (parseInt((startNum / cnt) * 100) < 100) {
+    $("#loading").text("Loading..." + parseInt((startNum / cnt) * 100) + "%");
+  }
+
+  // startNumがデータの総個数(results_available)を超えないように
   if (parseInt(startNum / 100) != parseInt(cnt / 100) + 1) {
     doAjax(url, range, startNum);
   } else {
+    $("#loading").text("Loading...100%");
     pagination(dataSet);
     return;
   }
@@ -60,6 +67,7 @@ function successCall(data) {
 
 // ページング処理
 /*Pagination Library - Normal(https://pagination.js.org/)*/
+//   使い方
 //       $('#demo').pagination({
 //     dataSource: [1, 2, 3, 4, 5, 6, 7, ... , 195],
 //     callback: function(data, pagination) {
