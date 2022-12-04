8cef561b3ada213cd32d4323aaa7d2e882169d1a
minji <busy8863@naver.com>
2022-12-04 21:24:31 +0900

    fixed code

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,3 +1,7 @@
+// 東京駅
+// lat=35.680930
+// lng=139.766863
+
 var latlng;
 function getUserLocation() {
   // Geolocation APIにアクセスできるかどうかを確認
@@ -9,8 +13,8 @@ function getUserLocation() {
       lat = pos.coords.latitude;
       lng = pos.coords.longitude;
 
-      // latlng = { lat: lat, lng: lng }; // real one
-      latlng = { lat: 35.68093, lng: 139.766863 }; // set Tokyo for testing
+      latlng = { lat: lat, lng: lng }; // real one
+      // latlng = { lat: 35.68093, lng: 139.766863 }; // set Tokyo for testing
 
       setMainShops(latlng);
     });
